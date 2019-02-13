921b409cb17858bd9cd00486807135d7d81e3ac0
dmitry <lanselot_2000@mail.ru>
2019-02-13 10:54:10 +0300

    third commit

--- task01/src/by/epam/task01/main/Main.java
+++ task01/src/by/epam/task01/main/Main.java
@@ -1,7 +1,12 @@
 package by.epam.task01.main;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LOGGER.info("Hello World!!!");
     }
 }

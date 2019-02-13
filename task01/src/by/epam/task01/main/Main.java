c6a80f7d4c2fbd34cde375be5171e2fbcdf660ee
dmitry <lanselot_2000@mail.ru>
2019-02-13 10:32:29 +0300

    second commit

--- task01/src/by/epam/task01/main/Main.java
+++ task01/src/by/epam/task01/main/Main.java
@@ -2,6 +2,6 @@ package by.epam.task01.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!!");
     }
 }
